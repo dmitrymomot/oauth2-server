6d0e4779fbb92e01423701157c05b6b421f2ab5f
Dmitry Momot <mail@dmomot.com>
2023-04-05 20:07:53 +0300

    go-kit middleware refactoring

--- lib/middleware/gokit.go
+++ lib/middleware/gokit.go
@@ -6,9 +6,9 @@ import (
 
 	"github.com/dmitrymomot/oauth2-server/internal/httpencoder"
 	"github.com/dmitrymomot/oauth2-server/lib/client"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-kit/kit/auth/jwt"
 	"github.com/go-kit/kit/endpoint"
-	"github.com/go-oauth2/oauth2/v4/errors"
 )
 
 // GokitAuthMiddleware is a middleware for gokit
@@ -17,12 +17,22 @@ func GokitAuthMiddleware(verifyFn VerifyTokenFunc) endpoint.Middleware {
 		return func(ctx context.Context, request interface{}) (response interface{}, err error) {
 			token, ok := ctx.Value(jwt.JWTContextKey).(string)
 			if token == "" || !ok {
-				return nil, httpencoder.NewError(http.StatusUnauthorized, errors.ErrInvalidAccessToken, "", nil)
+				return nil, httpencoder.ErrorResponse{
+					Code:      http.StatusUnauthorized,
+					Err:       "unauthorized",
+					Message:   "Missed or invalid access token",
+					RequestID: middleware.GetReqID(ctx),
+				}
 			}
 
 			info, err := verifyFn(token, client.TokenTypeAccessToken)
 			if err != nil || info == nil || !info.Active {
-				return nil, httpencoder.NewError(http.StatusUnauthorized, errors.ErrInvalidAccessToken, "", nil)
+				return nil, httpencoder.ErrorResponse{
+					Code:      http.StatusUnauthorized,
+					Err:       "unauthorized",
+					Message:   "Access token is invalid",
+					RequestID: middleware.GetReqID(ctx),
+				}
 			}
 
 			return next(SetTokenInfoToContext(ctx, info), request)
